a38a35c21c3d7a7cf05918a30ace9f1acffed22d
Omar Elweshy <omarelweshy@gmail.com>
2020-07-06 16:56:28 +0200

    Updates

--- pages/urls.py
+++ pages/urls.py
@@ -4,9 +4,9 @@ from .views import *
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('<slug:slug>/', CategoryReportsView.as_view(), name='categories_reports'),
-    path('/<uuid:pk>/', ReportDetailView.as_view(), name='report_detail'),
+    path('report/<uuid:pk>/', ReportDetailView.as_view(), name='report_detail'),
     path('report/create/', CreateReportView.as_view(), name='create_report'),
-    path('/<uuid:pk>/update/', UpdateReportView.as_view(), name='update_report'),
-    path('/<uuid:pk>/delete/', DeleteReportView.as_view(), name='delete_report'),
+    path('report/<uuid:pk>/update/', UpdateReportView.as_view(), name='update_report'),
+    path('report/<uuid:pk>/delete/', DeleteReportView.as_view(), name='delete_report'),
 
 ]
